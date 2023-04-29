4c8a8ef11c0895748e2e0ab11a4e500707a46a4f
Alec VanLandingham <alecvl08@vt.edu>
2023-04-29 16:42:39 -0400

    env var troubleshooting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 const BASE_URL = process.env.REACT_APP_API_BASE_PATH
-console.log(BASE_URL)
 
 const App: React.FC = () => {
     const [artist, setArtist] = useState('')
