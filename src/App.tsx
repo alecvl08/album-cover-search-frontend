4a0fc5890160d0314e26f8d728224d10f70f4fc8
Alec VanLandingham <alecvl08@vt.edu>
2023-04-29 15:38:58 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = process.env.API_BASE_PATH
+
+const App: React.FC = () => {
+    const [artist, setArtist] = useState('')
+    const [album, setAlbum] = useState('')
+    const [covers, setCovers] = useState<string[]>([])
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault()
+        try {
+            setLoading(true)
+            const response = await axios.get(`${BASE_URL}?artist=${encodeURIComponent(artist)}&album=${encodeURIComponent(album)}`)
+            setCovers(response.data)
+        } catch (error) {
+            console.error('Error:', error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <>
+            <section className="section has-background-success-dark">
+                <div className="columns is-centered">
+                    <div className="column">
+                            
+                    </div>
+                    <div className="column is-half has-text-centered">
+                        <h1 className="title is-1 has-text-white">Album Cover Search</h1>
+                    </div>
+                    <div className="column">
+                        
+                    </div>
+                </div>
+            </section>
+            <div className="container is-max-desktop">
+                <div className="box">
+                    <div className="form">
+                        <form onSubmit={handleSubmit}>
+                            <div className="field">
+                                <label className="label">Artist</label>
+                                <div className="control">
+                                    <input
+                                        className="input"
+                                        type="text"
+                                        value={artist}
+                                        onChange={event => setArtist(event.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label">Album</label>
+                                <div className="control">
+                                    <input
+                                        className="input"
+                                        type="text"
+                                        value={album}
+                                        onChange={event => setAlbum(event.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="control">
+                                    <button className="button is-primary" type="submit">
+                                        Search
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <div className="container">
+                {
+                    loading ? (
+                        <div className="box">
+                            Loading...
+                        </div>
+                    ) : (
+                        <div className="columns is-multiline">
+                            {
+                                covers.map(
+                                    (cover, index) => (
+                                        <div className="column is-3" key={index}>
+                                            <div className="box">
+                                                <img src={cover} alt="Album Cover"/>
+                                            </div>
+                                        </div>
+                                    )
+                                )
+                            }
+                        </div>
+                    )
+                }
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
